e037a3dced111b6af6d00c617ff313950e2f08e1
Sacha Bron <sacha.bron@copperflake.com>
2016-06-27 21:08:22 +0200

    fix folder exists bug

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/*', function (req, res) {
 var storage = multer.diskStorage({
 	destination: function (req, file, cb) {
 		var dir = './uploads/' + req.body.folder
-		fs.mkdir(dir, err => cb(err, dir))
+		fs.mkdir(dir, err => console.error(err))
 		cb(null, dir)
 	},
 	filename: function (req, file, cb) {
@@ -24,7 +24,7 @@ app.post('/', upload.single('fragment'), function (req, res, next) {
 	console.log(req.body)
 	console.log(req.file)
 	res.send('done')
-});
+})
 
 app.listen(3000,function(){
 	console.log("Listening on port 3000");
